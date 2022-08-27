a1ce130ac935433b5875b37debe819105025731e
ogmobot <ogmobot@github.com>
2022-08-27 10:54:16 +1000

    Simplify some expressions in supplementary day 21 program

--- day21-supplementary.c
+++ day21-supplementary.c
@@ -6,28 +6,16 @@
 int do_program(int64_t init) {
     int64_t r[6] = {init, 0, 0, 0, 0, 0};
     int counter = 0;
-    r[1] = 0;
 label_C:
     r[4] = r[1] | 65536;
     r[1] = 16298264;
 label_F:
-    r[5] = r[4] & 255;
-    r[1] = r[1] + r[5];
-    r[1] = r[1] & 16777215;
-    r[1] = r[1] * 65899;
-    r[1] = r[1] & 16777215;
-    r[5] = 256 > r[4];
-    if (r[5]) goto label_A;
+    r[1] = (((r[1] + (r[4] & 255)) & 16777215) * 65899) & 16777215;
+    if (256 > r[4]) goto label_A;
 /* label_B: */
-    r[5] = 0;
-label_D:
-    r[3] = r[5] + 1;
-    r[3] = r[3] * 256;
-    r[3] = r[3] > r[4];
-    if (r[3]) goto label_E;
-    r[5] = r[5] + 1;
-    goto label_D;
-label_E:
+    for (r[5] = 0; !((r[5] + 1) * 256 > r[4]); r[5]++)
+        ;
+/* label_E: */
     r[4] = r[5];
     goto label_F;
 label_A:
