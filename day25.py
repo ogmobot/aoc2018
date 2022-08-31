02496c781a9b3ce75c42b22c018487a3daf08919
ogmobot <ogmobot@github.com>
2022-08-31 22:10:02 +1000

    Day 25 solution

---
+++ day25.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+
+def manhattan(a, b):
+    return sum(map((lambda ax, bx: abs(ax - bx)), a, b))
+
+def constellate(stars):
+    # stars is a list of constellations
+    if not stars:
+        return []
+    first, rest = stars[0], stars[1:]
+    for i, candidate in enumerate(rest):
+        if any(manhattan(a, b) <= 3 for a in first for b in candidate):
+            return constellate(rest[:i] + [candidate + first] + rest[i + 1:])
+    return [first] + constellate(rest)
+
+def parse_star(s):
+    return tuple([int(i) for i in s.split(",")])
+
+def main(filename):
+    with open(filename, "r") as fp:
+        stars = [[parse_star(line)] for line in fp]
+    print(len(constellate(stars)))
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(3000) # lol
+    main("input25.txt")
