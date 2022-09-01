d40b457a1357157e01708dec56e98085206e60f3
ogmobot <ogmobot@github.com>
2022-09-01 20:13:45 +1000

    Adjust day 21 supplementary to solve both parts of puzzle

--- day21-supplementary.c
+++ day21-supplementary.c
@@ -37,35 +37,41 @@ int stack_contains(struct stack *s, int64_t value) {
 /* Translation of assembly program */
 int do_program(int64_t init, int32_t mode) {
     /* Mode 0: run normally
-       Mode 1: solve puzzle */
+     * Mode 1: solve part 1
+     * Mode 2: solve part 2
+     */
     struct stack *ns = (void *) 0;
     int64_t r[6] = {init, 0, 0, 0, 0, 0};
     int counter = 0;
-label_C:
+label_reset:
     r[4] = r[1] | 0x10000;
     r[1] = 16298264;
-label_F:
+label_loop:
     r[1] = (((r[1] + (r[4] & 0xFF)) & 0xFFFFFF) * 65899) & 0xFFFFFF;
-    if (256 > r[4]) goto label_A;
+    if (256 > r[4]) goto label_compare;
     for (r[5] = 0; !((r[5] + 1) * 0x100 > r[4]); r[5]++)
         ;
     r[4] = r[5];
-    goto label_F;
-label_A:
+    goto label_loop;
+label_compare:
     /*printf("%ld\n", r[1]);*/
-    if (mode == 1) {
+    switch (mode) {
+    case 0:
+        if (r[0] == r[1])
+            return counter;
+    case 1:
+        return r[1];
+    case 2:
         if (stack_contains(ns, r[1])) {
             int res = ns->value;
             free_stack(ns);
             return res;
         }
         push_stack(&ns, r[1]);
-    } else {
-        if (r[0] == r[1])
-            return counter;
+        break;
     }
     counter++;
-    goto label_C;
+    goto label_reset;
 }
 
 int main(void) {
@@ -76,6 +82,6 @@ int main(void) {
     int res = do_program(init);
     printf("This number was reached after %d others.\n", res);
     */
-    int res = do_program(0, 1);
-    printf("%d\n", res);
+    printf("%d\n", do_program(0, 1));
+    printf("%d\n", do_program(0, 2));
 }
