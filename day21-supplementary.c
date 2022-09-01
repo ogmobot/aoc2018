166c1bec9b301c4e0f39fd482890264869265b1d
ogmobot <ogmobot@github.com>
2022-09-01 20:22:12 +1000

    Day 21 supp. -- rename variables to be less mysterious

--- day21-supplementary.c
+++ day21-supplementary.c
@@ -41,33 +41,33 @@ int do_program(int64_t init, int32_t mode) {
      * Mode 2: solve part 2
      */
     struct stack *ns = (void *) 0;
-    int64_t r[6] = {init, 0, 0, 0, 0, 0};
+    int64_t random_val = 0, salt, tmp;
     int counter = 0;
 label_reset:
-    r[4] = r[1] | 0x10000;
-    r[1] = 16298264;
+    salt = random_val | 0x10000;
+    random_val = 16298264;
 label_loop:
-    r[1] = (((r[1] + (r[4] & 0xFF)) & 0xFFFFFF) * 65899) & 0xFFFFFF;
-    if (256 > r[4]) goto label_compare;
-    for (r[5] = 0; !((r[5] + 1) * 0x100 > r[4]); r[5]++)
+    random_val = (((random_val + (salt & 0xFF)) & 0xFFFFFF) * 65899) & 0xFFFFFF;
+    if (256 > salt) goto label_compare;
+    for (tmp = 0; !((tmp + 1) * 0x100 > salt); tmp++)
         ;
-    r[4] = r[5];
+    salt = tmp;
     goto label_loop;
 label_compare:
     /*printf("%ld\n", r[1]);*/
     switch (mode) {
     case 0:
-        if (r[0] == r[1])
+        if (init == random_val)
             return counter;
     case 1:
-        return r[1];
+        return random_val;
     case 2:
-        if (stack_contains(ns, r[1])) {
+        if (stack_contains(ns, random_val)) {
             int res = ns->value;
             free_stack(ns);
             return res;
         }
-        push_stack(&ns, r[1]);
+        push_stack(&ns, random_val);
         break;
     }
     counter++;
